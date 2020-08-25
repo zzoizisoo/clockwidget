014bc414e8e79fc9801464e580f247111349a0a4
zzoizisoo <zzoizi@zzoizisooui-iMac.local>
2020-08-25 17:17:25 +0900

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import moment from 'moment';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let intervalID;
   const [time, setTime] = useState("00:00 am")
 
   useEffect(()=>{
-    intervalID = setInterval(()=>setTime(moment().format('hh:mm a'))
+    const intervalID = setInterval(()=>setTime(moment().format('hh:mm a'))
     , 5000)
     return ()=>clearInterval(intervalID) 
   }, [])
