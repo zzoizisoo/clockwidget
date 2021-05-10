09d6ee6c8d5102fa8421485deee03ccfcdaed5d7
zzoizisoo <zzoizi@zzoizisooui-iMac.local>
2021-05-10 14:42:48 +0900

    chagne time format

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import moment from 'moment';
 import './App.css';
 
 function App() {
-  const [time, setTime] = useState("00:00 am")
+  const [time, setTime] = useState(null)
 
   useEffect(()=>{
-    const intervalID = setInterval(()=>setTime(moment().format('hh:mm a'))
+    const intervalID = setInterval(()=>setTime(moment().format('HH:mm'))
     , 5000)
     return ()=>clearInterval(intervalID) 
   }, [])
@@ -14,7 +14,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="time">{time}</div>
+        { time && <div className="time">{time} KST</div>}
+        
       </header>
     </div>
   );
