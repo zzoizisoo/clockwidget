985aac9526f8206a1fa0d98f963bcc30f54e4b58
Jisu Choi <zzoizisoo@gmail.com>
2021-10-01 17:43:56 +0900

    style change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import './App.css';
+import {FaTwitch} from 'react-icons/fa';
+import {IoMdHeart} from 'react-icons/io';
+import {AiFillSound} from 'react-icons/ai';
 
 function App() {
-  const [time, setTime] = useState(null)
+	const [time, setTime] = useState(null);
 
-  useEffect(()=>{
-    const intervalID = setInterval(()=>setTime(moment().format('HH:mm'))
-    , 5000)
-    return ()=>clearInterval(intervalID) 
-  }, [])
+	const iconStyle = {
+		marginRight: '15px'
+	};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        { time && <div className="time">{time} KST</div>}
-        
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		const intervalID = setInterval(() => setTime(moment().format('YYYY MMM Do (ddd) HH:mm')), 5000);
+		return () => clearInterval(intervalID);
+	}, []);
+
+	return (
+		<div className='App'>
+			<div style={{fontWeight: 'bold', display: 'flex', alignItems: 'center'}}>
+				<FaTwitch style={iconStyle} />
+				<div>JEZZI</div>
+			</div>
+			<div style={{display: 'flex', alignItems: 'center'}}>
+				<IoMdHeart style={iconStyle} />
+				<AiFillSound style={iconStyle} />
+
+				{time && <div className='time'>{time} KST</div>}
+			</div>
+		</div>
+	);
 }
 
 export default App;
